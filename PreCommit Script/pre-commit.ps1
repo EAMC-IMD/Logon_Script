64fc30886f43f45e7a6b536d44c71f315c316ab5
sapph42 <31635854+sapph42@users.noreply.github.com>
2025-03-15 19:51:19 -0400

    fix typo

--- PreCommit Script/pre-commit.ps1
+++ PreCommit Script/pre-commit.ps1
@@ -8,10 +8,10 @@ $SiteCode = 'EAMC' # $env:COMPUTERNAME.Substring(0,4)
 $regex = "(`"|')(?<redact>(\\\\)?$SiteCode[^`"']+)(`"|')"
 
 # Define script file to sanitize (only staged version, not working copy)
-$scriptPath = "$PSScriptRoot\..\SOURCE\login.ps1"
+$scriptPath = "$PSScriptRoot\..\SOURCE\logon.ps1"
 
 # Get staged content
-$stagedContent = git show :SOURCE/login.ps1
+$stagedContent = git show :SOURCE/logon.ps1
 
 # Sanitize the content
 $sanitizedContent = $stagedContent -ireplace $regex, "`$(throw `"Value has been redacted. Replace this with local value.`")"
@@ -20,7 +20,7 @@ $sanitizedContent = $stagedContent -ireplace $regex, "`$(throw `"Value has been 
 $sanitizedContent | Out-File $scriptPath -Encoding utf8 -Force -NoNewline
 
 # Re-add the sanitized file to the commit (but leave the working copy unchanged)
-git add SOURCE/Login.ps1
+git add SOURCE/logon.ps1
 
 Write-Host "Pre-commit sanitization completed. Sensitive values redacted in commit."
 exit 0
